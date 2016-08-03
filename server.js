6fda097218163ebefd793e44cdeffa96fe1a0e33
ghosteins12 <gaspard.hosteins@twelve-consulting.com>
2016-08-03 17:41:43 +0200

    0.0.2

--- server.js
+++ server.js
@@ -20,19 +20,7 @@ var express = require('express'),
     res.end();
 });
 server.listen(8080);*/
-var server = http.createServer(function(req, res) {
-    var page = url.parse(req.url).pathname;
-    console.log(page);
-    res.writeHead(200, {"Content-Type": "html"});
-    res.write('<a href="https://slack.com/oauth/authorize?'
-    +'scope=bot,incoming-webhook,commands,identify,channels:history,channels:read,channels:write,chat:write:bot,chat:write:user,file:read,'
-    +'file:write:user,groups:history,groups:read,groups:write,identity:basic,im:history,im:read,im:write,mpim:history,mpim:read,mpim:write,'
-    +'search:read,team:read,usergroups:write,usergroups:read,user:profile:read,user:profile:write,user:read,user:write&client_id=48833382512.61501878338">'
-    +'img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" '
-    +'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
-    res.end();
-});
-server.listen(8080);
+
 
 var app = express();
 
@@ -40,7 +28,12 @@ app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
-    var result = 'App is running'
+    var result = '<a href="https://slack.com/oauth/authorize?'
+    +'scope=bot,incoming-webhook,commands,identify,channels:history,channels:read,channels:write,chat:write:bot,chat:write:user,file:read,'
+    +'file:write:user,groups:history,groups:read,groups:write,identity:basic,im:history,im:read,im:write,mpim:history,mpim:read,mpim:write,'
+    +'search:read,team:read,usergroups:write,usergroups:read,user:profile:read,user:profile:write,user:read,user:write&client_id=48833382512.61501878338">'
+    +'img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" '
+    +'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
     response.send(result);
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
