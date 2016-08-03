e24d2af00c084bf9f023f9102bbf74802bf114dc
ghosteins12 <gaspard.hosteins@twelve-consulting.com>
2016-08-03 15:27:48 +0200

    0.0.1

--- server.js
+++ server.js
@@ -20,8 +20,14 @@ var express = require('express'),
     res.end();
 });
 server.listen(8080);*/
+var app     = express();
 
-var app = express();
-app.set('port', process.env.PORT || 5000);
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('port', (process.env.PORT || 5000));
 
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
