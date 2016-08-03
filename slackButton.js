f3f848da2f5a2fa1ffd0c598d0b8c53c37dd829f
ghosteins12 <gaspard.hosteins@twelve-consulting.com>
2016-08-03 17:39:38 +0200

    0.0.2

--- slackButton.js
+++ slackButton.js
@@ -1,4 +1,3 @@
-//appels aux librairies
 var express = require('express'),
     bodyParser = require('body-parser'),
     //moment = require('moment'),
@@ -12,14 +11,6 @@ var express = require('express'),
 
 //moment.locale('fr');
 
-/*var server = http.createServer(function(req, res) {
-    var page = url.parse(req.url).pathname;
-    console.log(page);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write('Bien le bonjour');
-    res.end();
-});
-server.listen(8080);*/
 var server = http.createServer(function(req, res) {
     var page = url.parse(req.url).pathname;
     console.log(page);
@@ -33,15 +24,3 @@ var server = http.createServer(function(req, res) {
     res.end();
 });
 server.listen(8080);
-
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
