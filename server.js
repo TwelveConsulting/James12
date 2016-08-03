c206917cef0877444f8a87c3b47d42ebfefcb53f
ghosteins12 <gaspard.hosteins@twelve-consulting.com>
2016-08-03 17:47:16 +0200

    0.0.2

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/', function(request, response) {
     var result = '<a href="https://slack.com/oauth/authorize?'
     +'scope=bot,incoming-webhook,commands,identify,channels:history,channels:read,channels:write,chat:write:bot,chat:write:user,files:read,'
     +'files:write:user,groups:history,groups:read,groups:write,identity.basic,im:history,im:read,im:write,mpim:history,mpim:read,mpim:write,'
-    +'search:read,team:read,usergroups:write,usergroups:read,users:profile:read,users:profile:write,users:read,users:write&client_id=48833382512.61501878338">'
+    +'search:read,team:read,usergroups:write,usergroups:read,users.profile:read,users.profile:write,users:read,users:write&client_id=48833382512.61501878338">'
     +'img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" '
     +'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
     response.send(result);
