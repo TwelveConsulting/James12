e78fc5474c6606d386a0cb15e2a11f8272b8a763
ghosteins12 <gaspard.hosteins@twelve-consulting.com>
2016-08-04 12:10:17 +0200

    0.0.2

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 //appels aux librairies
 var express = require('express'),
     bodyParser = require('body-parser'),
-    //moment = require('moment'),
+    moment = require('moment'),
     http = require("http"),
     url = require("url");//,
-    //Botkit = require('botkit'),
+    Botkit = require('botkit'),
+    conges = require('./modules/conges'),
     //Store = require("jfs"),
     //fs = require("fs"),
     //path = require("path"),
     //mime = require("mime");
 
-//moment.locale('fr');
-
-/*var server = http.createServer(function(req, res) {
-    var page = url.parse(req.url).pathname;
-    console.log(page);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write('Bien le bonjour');
-    res.end();
-});
-server.listen(8080);*/
-
+moment.locale('fr');
 
 var app = express();
 
@@ -57,7 +48,7 @@ app.get('/', function(request, response) {
                                                                 +'users:write'
                                                                 +'&client_id=48833382512.61501878338">'
                     +'<img alt="Add to Slack" height="40" width="139"' 
-                         +'src="https://platform.slack-edge.com/img/add_to_slack.png" '
+                        +'src="https://platform.slack-edge.com/img/add_to_slack.png" '
                         +'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, '
                         +'https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />'
                 +'</a>'
@@ -65,3 +56,5 @@ app.get('/', function(request, response) {
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
+
+app.post('/conges', conges.execute);
