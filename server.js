804747f982edb61d962a1ba294aa325d8932457a
ghosteins12 <gaspard.hosteins@twelve-consulting.com>
2016-08-16 12:01:49 +0200

    test

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/', function(request, response) {
                                                                 +'&client_id='+process.env.CLIENT_ID+'>'
                         +'<img alt="Add to Slack" height="40" width="139"'
                         +'src="https://platform.slack-edge.com/img/add_to_slack.png" '
-                        +'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
+                        +'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>';
     response.send(result);
 }).listen(port, function () {
   console.log('Ready');
