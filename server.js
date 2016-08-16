1c2479825d6ac2d20c38edaf44e9dbd8106ca303
ghosteins12 <gaspard.hosteins@twelve-consulting.com>
2016-08-16 16:03:52 +0200

    test

--- server.js
+++ server.js
@@ -22,70 +22,59 @@ var port = process.env.PORT || 5000;
 
 //Fonctions de Callback
 boutonSlack = function(req, res, next) {
-    res.send('<a href="https://slack.com/oauth/authorize?scope=bot,'
-                                                                +'incoming-webhook,'
-                                                                +'commands,'
-                                                                +'identify,'
-                                                                +'channels:history,'
-                                                                +'channels:read,'
-                                                                +'channels:write,'
-                                                                +'chat:write:bot,'
-                                                                +'chat:write:user,'
-                                                                +'files:read,'
-                                                                +'files:write:user,'
-                                                                +'groups:history,'
-                                                                +'groups:read,'
-                                                                +'groups:write,'
-                                                                +'im:history,'
-                                                                +'im:read,'
-                                                                +'im:write,'
-                                                                +'mpim:history,'
-                                                                +'mpim:read,'
-                                                                +'mpim:write,'
-                                                                +'search:read,'
-                                                                +'team:read,'
-                                                                +'usergroups:write,'
-                                                                +'usergroups:read,'
-                                                                +'users.profile:read,'
-                                                                +'users.profile:write,'
-                                                                +'users:read,'
-                                                                +'users:write'
-                                                                +'&client_id='+process.env.CLIENT_ID+'">' 
-                        +'<img alt="Add to Slack" height="40" width="139"'
-                        +'src="https://platform.slack-edge.com/img/add_to_slack.png" '
-                        +'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, '
-                        +'https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
-    console.log('cb0:le bouton slack s\'affiche');
-    res.end();
+    if (req.params == {}) {
+        res.send('<a href="https://slack.com/oauth/authorize?scope=bot,'
+                                                            +'incoming-webhook,'
+                                                            +'commands,'
+                                                            +'identify,'
+                                                            +'channels:history,'
+                                                            +'channels:read,'
+                                                            +'channels:write,'
+                                                            +'chat:write:bot,'
+                                                            +'chat:write:user,'
+                                                            +'files:read,'
+                                                            +'files:write:user,'
+                                                            +'groups:history,'
+                                                            +'groups:read,'
+                                                            +'groups:write,'
+                                                            +'im:history,'
+                                                            +'im:read,'
+                                                            +'im:write,'
+                                                            +'mpim:history,'
+                                                            +'mpim:read,'
+                                                            +'mpim:write,'
+                                                            +'search:read,'
+                                                            +'team:read,'
+                                                            +'usergroups:write,'
+                                                            +'usergroups:read,'
+                                                            +'users.profile:read,'
+                                                            +'users.profile:write,'
+                                                            +'users:read,'
+                                                            +'users:write'
+                                                            +'&client_id='+process.env.CLIENT_ID+'">' 
+                    +'<img alt="Add to Slack" height="40" width="139"'
+                    +'src="https://platform.slack-edge.com/img/add_to_slack.png" '
+                    +'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, '
+                    +'https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
+        console.log('cb0:le bouton slack s\'affiche');
+        res.end();
+    }
     next();
 };
 
 recupCode = function(req, res, next){
-    var code = req.param('code');
-    res.send(code);
-    console.log('cb1 : le code est récupéré');
+    if (!(req.params == {})) {
+        var code = req.param('code');
+        res.send(code);
+        console.log('cb1 : le code est récupéré');
+    }
     next(); 
 };
 
-app.get('/', function (req, res, next) {
-  // if the user ID is 0, skip to the next route
-  if (req.params == {}) next('route');
-  // otherwise pass the control to the next middleware function in this stack
-  else next(); //
-}, function (req, res, next) {
-  // render a regular page
-  boutonSlack;
-});
-
-// handler for the /user/:id path, which renders a special page
-app.get('/user/:id', function (req, res, next) {
-  recupCode;
-});
-
-/*app.get('/', [boutonSlack,recupCode]);
+app.get('/', [boutonSlack,recupCode]);
 app.listen(port, function () {
   console.log('Ready');
-});*/
+});
 
 
 app.post('/conges', conges.execute);
