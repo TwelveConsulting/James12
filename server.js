b68d82e3db61be816356479cc549f509ef73259a
ghosteins12 <gaspard.hosteins@twelve-consulting.com>
2016-08-22 10:18:51 +0200

    test

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var port = process.env.PORT || 5000;
 
 //Fonctions de Callback
 boutonSlack = function(req, res,next) {
-    res.send('<a href="https://slack.com/oauth/authorize?scope=bot,'
+    https.get('https://slack.com/oauth/authorize?scope=bot,'
                                                         +'incoming-webhook,'
                                                         +'commands,'
                                                         +'identify,'
@@ -50,11 +50,7 @@ boutonSlack = function(req, res,next) {
                                                         +'users.profile:write,'
                                                         +'users:read,'
                                                         +'users:write'
-                                                        +'&client_id='+process.env.CLIENT_ID+'">' 
-                +'<img alt="Add to Slack" height="40" width="139"'
-                +'src="https://platform.slack-edge.com/img/add_to_slack.png" '
-                +'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, '
-                +'https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
+                                                        +'&client_id='+process.env.CLIENT_ID)
     console.log('cb0:le bouton slack s\'affiche');
     next();
     app.get('/reponseOAuth/',recupCode);
